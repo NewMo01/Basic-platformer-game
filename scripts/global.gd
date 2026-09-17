3114287882b41c62d47e59228349d8e62aa12253
NewMo01 <mo.medhat99@gmail.com>
2026-09-17 18:35:47 +0300

    update main scene and global

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,3 +1,20 @@
 extends Node
 
-print(AudioServer)
+var gameOver := false
+var gameWin := false
+var activePowerUp := false
+var health := 3
+var coins := 0
+var isInBossBattle := false
+var defeatedBoss := false
+
+
+func resetValues():
+	gameOver = false
+	gameWin = false
+	activePowerUp = false
+	health = 3
+	coins = 0
+	isInBossBattle = false
+	defeatedBoss = false
+	AudioServer.set_bus_mute(0,false)
